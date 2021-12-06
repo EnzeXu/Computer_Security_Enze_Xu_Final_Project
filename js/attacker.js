798ca1c5d893e32398b8a5cde9c0009e2bf49687
EnzeXu <xue20@wfu.edu>
2021-12-06 05:26:47 -0500

    sth

--- js/attacker.js
+++ js/attacker.js
@@ -1,1 +1,5 @@
 console.log("Hello I am the attacker!");
+console.log(document.all[37].attributes[0].nodeValue);
+console.log(document.all[37].attributes[0].nodeValue);
+let samy_id = 47;
+console.log(document.cookie);
